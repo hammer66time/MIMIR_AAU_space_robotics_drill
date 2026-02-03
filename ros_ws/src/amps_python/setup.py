50ed132bc67c4631fcc6189dc7a42d22ca4c31e4
Petur <phamme24@student.aau.dk>
2026-02-03 17:34:41 +0100

    Refactor GUI structure and update entry point for main application

--- ros_ws/src/amps_python/setup.py
+++ ros_ws/src/amps_python/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'GUI = amps_python.GUI:main',
+            'gui = amps_python.main:main',
         ],
     },
 )
